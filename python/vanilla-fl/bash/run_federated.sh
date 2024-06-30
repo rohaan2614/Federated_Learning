49d938a2b0e9107052f27fcbe607ae52889a1319
roan <rohaan.nadeem2614@gmail.com>
2024-06-30 16:00:32 -0400

    bash to run scripts 10x

--- python/vanilla-fl/bash/run_federated.sh
+++ python/vanilla-fl/bash/run_federated.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Create a logs directory if it doesn't exist
-mkdir -p logs/baseline
+mkdir -p logs/federated
 
 # Loop to run the command 10 times
 for i in {1..10}
 do
     echo "Running iteration $i..."
     # Run the command and store the output in a log file
-    poetry run python src/baseline.py > logs/baseline/run_$i.log 2>&1
+    poetry run python src/federated.py > logs/federated/run_$i.log 2>&1
 done
 
-echo "All iterations completed. Logs are saved in the 'logs/baseline' directory."
+echo "All iterations completed. Logs are saved in the 'logs/federated' directory."
